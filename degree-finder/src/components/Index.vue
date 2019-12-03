eced9b8316d92388ed497f1965fa30bd8c82b9ee
Jacob Bryant <jacobryas4@gmail.com>
2019-12-03 14:13:49 -0500

    new index

---
+++ degree-finder/src/components/Index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <h1 class="p-3">Choose how to search</h1>
+        <div class="row">
+            <div class="card m-3" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">By Category</h5>
+                    <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                    <p class="card-text">Search for degrees and their requirements by category</p>
+                    <a href="#" class="card-link">Begin Search</a>
+                </div>
+            </div>
+            <div class="card m-3" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">By Degree</h5>
+                    <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                    <p class="card-text">Get degree requirements by a particular degree field</p>
+                    <a href="#" class="card-link">Begin Search</a>
+                </div>
+            </div>
+            <div class="card m-3" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">By School</h5>
+                    <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                    <p class="card-text">Search for degrees and their requirements by School Partner</p>
+                    <a href="#" class="card-link">Begin Search</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    data () {
+        return {
+
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
